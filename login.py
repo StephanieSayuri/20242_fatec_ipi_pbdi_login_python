d1509ce2b73ca30ae7edd2462edec1f9114d26f0
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2024-09-14 08:26:01 -0300

    implementa a funcao insere

--- login.py
+++ login.py
@@ -26,8 +26,29 @@ def existe(usuario):
   # devolver true ou false, conforme o resultado
   return result != None
 
+def insere(usuario):
+  # abrir uma conexão com o postgresql
+  with psycopg.connect(
+    host = "docker-db-1",
+    dbname = "pbdi",
+    user = "postgres",
+    password = "123456"
+  ) as conexao:
+    # por meio da conexão, obter uma abstração do tipo cursor
+    with conexao.cursor() as cursor:
+      # por meio do cursor, executar um comando insert
+      cursor.execute( #in-place
+        "insert into tb_usuario (login, senha) values (%s, %s);",
+        (usuario.login, usuario.senha)
+      )
+      # usar um método do cursor para verifiar o resultado produzido pelo insert
+      result = cursor
+
+  # devolver true ou false, conforme o resultado
+  return result != None
+
 def menu():
-  texto = "0-Fechar\n1-Login\n2-Logoff\n"
+  texto = "0-Fechar\n1-Login\n2-Logoff\n3-Inserir Usuário\n"
   usuario = None
   op = int(input(texto))
   while op!= 0:
@@ -39,6 +60,15 @@ def menu():
     elif op == 2:
       usuario = None
       print("Logoff realizado com sucesso")
+    elif op == 3:
+      login = input("Digite o login\n")
+      senha = input("Digite a senha\n")
+      usuario = Usuario(login, senha)
+      result = insere(usuario)
+      if result:
+        print("Usuário cadastrado com sucesso")
+      else:
+        print("Ocorreu um erro")
     op = int(input(texto))
   else:
     print("Até mais")
